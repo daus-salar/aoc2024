04878775946b1767d9e17a08e1ea7870122b9800
Lars Sadau <lars@sadau-online.de>
2024-12-05 03:22:12 +0100

    create regex outside for loop

--- src/day3.rs
+++ src/day3.rs
@@ -17,14 +17,14 @@ pub fn part2(input: &str) -> i32 {
 
 fn parse(input: &str) -> Result<Vec<Operation>, Error> {
     let ops_pattern = regex::Regex::new(r"mul\([0-9]+,[0-9]+\)|don't\(\)|do\(\)").unwrap();
+    let mul_pattern = regex::Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
 
     let mut result: Vec<Operation> = vec![];
     for c in ops_pattern.captures_iter(input) {
         match &c[0] {
             "do()" => result.push(Operation::Do()),
             "don't()" => result.push(Operation::DoNot()),
-            mul => {
-                let mul_pattern = regex::Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+            mul => {            
                 if let Some((_, [op1, op2])) = mul_pattern.captures(mul).map(|c| c.extract()) {
                     let op1: i32 = op1.parse().unwrap();
                     let op2: i32 = op2.parse().unwrap();
